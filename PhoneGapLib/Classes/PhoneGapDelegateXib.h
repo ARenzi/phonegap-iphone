bb6a9064766f72e3c840e0d854c35216880c993d
Christian Stocker <me@chregu.tv>
2010-06-22 16:50:51 +0200

    Get .xib stuff from master branch

---
+++ PhoneGapLib/Classes/PhoneGapDelegateXib.h
@@ -0,0 +1,18 @@
+//
+//  PhoneGapDelegate2.h
+//  tmpPhonegap2
+//
+//  Created by Christian Stocker on 10.02.10.
+//  Copyright 2010 Liip AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PhoneGapDelegate.h"
+
+@interface PhoneGapDelegateXib : PhoneGapDelegate {
+
+}
+
+
+
+@end
